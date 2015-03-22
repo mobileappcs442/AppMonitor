4c60d163fdc479258874e66c7ece97112da0aaa7
spate141 <spate141@hawk.iit.edu>
2015-03-22 15:46:31 -0500

    Added a print line (Snehal)

--- myapplication/src/main/java/com/cs442project/appmonitor/loader/AppEntry.java
+++ myapplication/src/main/java/com/cs442project/appmonitor/loader/AppEntry.java
@@ -32,6 +32,7 @@ public class AppEntry {
   }
 
   public Drawable getIcon() {
+      System.out.println("helo");
     if (mIcon == null) {
       if (mApkFile.exists()) {
         mIcon = mInfo.loadIcon(mLoader.mPm);
